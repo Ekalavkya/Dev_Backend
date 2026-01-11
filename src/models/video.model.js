129df7245696ab4fdb04e0b508ee2fbbb3050d8c
Ekalavkya <mihsan.aloyd@gmail.com>
2026-01-11 18:13:18 +0545

    The final changes of the user.model and video.model is made

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String,// cloudinary url
+            required:true
+        },
+        title: {
+            type: String,// cloudinary url
+            required:true
+        },
+        description: {
+            type: String,// cloudinary url
+            required:true
+        },
+        duration: {
+            type: Number,// cloudinary url
+            required:true
+        },
+        view: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,// this is the flag use whether the video is published or not.
+            required:true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+
+},
+    {
+    timestamps: true
+    }) 
+    videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model ("Video", videoSchema)
