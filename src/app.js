e34c017a7e7c9c60ebd2865e5891d2d3d6d530db
Ekalavkya <mihsan.aloyd@gmail.com>
2026-01-11 18:08:34 +0545

    adding user and video model

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 import express from "express"
+import cors from 'cors'
+import cookieParsal from "cookies-parser"
+
 const app = express()
-export {app}
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true 
+}))
+app.use(express.json({ limit: "18kb" }))
+app.use(express.urlencoded({ extended: true, limit: "18kb" }))
+app.use(express.static("public"))
+app.use (cookieParsal())
+export { app }
